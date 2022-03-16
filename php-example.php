c7a9a9b68ecffdc1713a5af81ba4638d5ecd7e28
Laim <14845036+Laim@users.noreply.github.com>
2022-03-16 01:24:46 +0000

    Update php-example.php

--- php-example.php
+++ php-example.php
@@ -3,7 +3,7 @@
 //Date: 07 Sept 2020
 
 <?php
-    $json = file_get_contents('https://ext.laim.scot/snowapi/api/1.0/?format=json');
+    $json = file_get_contents('https://snwapi.com/api/1.0/?format=json');
     $obj = json_decode($json, true);
 
     foreach ($obj as $i) {
